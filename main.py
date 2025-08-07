dd40998f08c65cb8b54f8dbc401f46fc0500e328
mark <markgamal2001@gmail.com>
2025-08-07 11:11:38 +0200

    updated flask port

--- main.py
+++ main.py
@@ -88,4 +88,6 @@ if __name__ == '__main__':
     if not os.getenv("GOOGLE_API_KEY"):
         print("Warning: GOOGLE_API_KEY not found in environment variables")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000) 
+
+
